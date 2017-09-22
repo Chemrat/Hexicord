2bc1daf27255a98f41d8035a99972a13bfc40175
Maks Mazurov (fox.cpp) <foxcpp@yandex.ru>
2017-09-23 01:22:20 +0300

    Add Channel struct

---
+++ src/hexicord/types/channel.cpp
@@ -0,0 +1,142 @@
+#include "hexicord/types/channel.hpp"
+#include <boost/variant/get.hpp>
+
+namespace Hexicord {
+
+BaseGuildChannel::BaseGuildChannel(const nlohmann::json& json)
+    : id                   (json.at    ("id")       .get<std::string>())
+    , guildId              (json.count("guild_id") != 0 ? json.value("guild_id", "0") : "0")
+    , categoryId           (json.count("parent_id") != 0 && !json["parent_id"].is_null() ? json.value ("parent_id", "0") : "0")
+    , name                 (json.at    ("name")     .get<std::string>())
+    , position             (json.at    ("position") .get<unsigned>()   )
+    , permissionOverwrites (json.at    ("permission_overwrites").get<std::vector<PermissionsOverwrite>>()) {}
+
+nlohmann::json BaseGuildChannel::dump() const {
+    nlohmann::json result = {
+        { "id",                    std::to_string(id)       },
+        { "name",                  name                     },
+        { "position",              position                 },
+        { "permission_overwrites", permissionOverwrites     }
+    };
+
+    if (categoryId != 0) result["parent_id"] = std::to_string(categoryId);
+    if (guildId    != 0) result["guild_id"]  = std::to_string(guildId);
+    return result;
+}
+
+TextChannel::TextChannel(const nlohmann::json& json)
+    : BaseGuildChannel(json)
+    , topic         (json["topic"] != nullptr ? json.at("topic").get<std::string>() : "")
+    , lastMessageId (json["last_message_id"].is_null() ? "0" : json.at("last_message_id").get<std::string>()) {}
+
+nlohmann::json TextChannel::dump() const {
+    nlohmann::json json = BaseGuildChannel::dump();
+
+    json["topic"]           = topic.empty() ? nlohmann::json(nullptr) : nlohmann::json(topic);
+    json["last_message_id"] = lastMessageId != 0 ? nlohmann::json(std::to_string(lastMessageId)) : nlohmann::json(nullptr);
+
+    return json;
+}
+
+VoiceChannel::VoiceChannel(const nlohmann::json& json)
+    : BaseGuildChannel(json)
+    , bitrate       (json.at("bitrate")    .get<unsigned>())
+    , usersLimit    (json.at("user_limit").get<unsigned>()) {}
+
+
+nlohmann::json VoiceChannel::dump() const {
+    nlohmann::json json = BaseGuildChannel::dump();
+
+    json["bitrate"]     = bitrate;
+    json["user_limit"] = usersLimit;
+
+    return json;
+}
+
+Category::Category(const nlohmann::json& json)
+    : BaseGuildChannel(json)
+    , nsfw (json.value("nsfw", false)) {}
+
+nlohmann::json Category::dump() const {
+    nlohmann::json result = BaseGuildChannel::dump();
+
+    result["nsfw"] = nsfw;
+
+    return result;
+}
+
+DmChannel::DmChannel(const nlohmann::json& json)
+    : id             (json.at("id")              .get<std::string>())
+    , lastMessageId  (json["last_message_id"].is_null() ? "0" : json.at("last_message_id").get<std::string>())
+    , recipient      (json.at("recipients")[0]   .get<User>()       ) {}
+
+nlohmann::json DmChannel::dump() const {
+    nlohmann::json json = {
+        { "id", uint64_t(id)                },
+        { "last_message_id", lastMessageId  },
+        { "recipients", { recipient.dump() }}
+    };
+
+    json["last_message_id"] = lastMessageId != 0 ? nlohmann::json(std::to_string(lastMessageId)) : nlohmann::json(nullptr);
+
+    return json;
+}
+
+GroupDmChannel::GroupDmChannel(const nlohmann::json& json)
+    : id             (json.at("id")              .get<std::string>()      )
+    , lastMessageId  (json["last_message_id"].is_null() ? "0" : json.at("last_message_id").get<std::string>())
+    , recipients     (json.at("recipients")      .get<std::vector<User>>()) {}
+
+nlohmann::json GroupDmChannel::dump() const {
+    nlohmann::json json = {
+        { "id", uint64_t(id)               },
+        { "recipients", recipients         }
+    };
+
+    json["last_message_id"] = lastMessageId != 0 ? nlohmann::json(std::to_string(lastMessageId)) : nlohmann::json(nullptr);
+
+    return json;
+}
+
+ChannelType channelType(const nlohmann::json& json) {
+    return static_cast<ChannelType>(json.at("type").get<unsigned>());
+}
+
+void from_json(const nlohmann::json& json, Channel& channel) {
+    ChannelType type = channelType(json);
+    switch (type) {
+    case ChannelType::Text:
+        channel = Channel(TextChannel(json));
+        break;
+    case ChannelType::Voice:
+        channel = Channel(VoiceChannel(json));
+        break;
+    case ChannelType::Category:
+        channel = Channel(Category(json));
+        break;
+    case ChannelType::Dm:
+        channel = Channel(DmChannel(json));
+        break;
+    case ChannelType::GroupDm:
+        channel = Channel(GroupDmChannel(json));
+        break;
+    }
+}
+
+void to_json(nlohmann::json& json, const Channel& channel) {
+    if (channel.type() == typeid(TextChannel)) {
+        json = boost::get<TextChannel>(channel);
+    } else if (channel.type() == typeid(VoiceChannel)) {
+        json = boost::get<VoiceChannel>(channel);
+    } else if (channel.type() == typeid(Category)) {
+        json = boost::get<Category>(channel);
+    } else if (channel.type() == typeid(DmChannel)) {
+        json = boost::get<DmChannel>(channel);
+    } else if (channel.type() == typeid(GroupDmChannel)) {
+        json = boost::get<GroupDmChannel>(channel);
+    } else {
+        assert(false);
+    }
+}
+
+} // namespace Hexicord
---
+++ src/hexicord/types/channel.hpp
@@ -0,0 +1,155 @@
+#ifndef HEXICORD_TYPES_CHANNEL_HPP
+#define HEXICORD_TYPES_CHANNEL_HPP
+
+#include <boost/variant/variant.hpp>
+#include "hexicord/json.hpp"
+#include "hexicord/types/nlohmann_helper.hpp"
+#include "hexicord/types/snowflake.hpp"
+#include "hexicord/types/user.hpp"
+#include "hexicord/types/role.hpp"
+
+namespace Hexicord {
+    struct BaseGuildChannel {
+        /// Channel ID.
+        Snowflake id;
+
+        /// Guild ID. Can be 0 if not present in JSON
+        /// it's constructed from (for example channels from GuildCreate event).
+        Snowflake guildId;
+
+        /// The ID of parent category, if any, 0 otherwise.
+        Snowflake categoryId;
+
+        /// The name of the channel (2-100 characters).
+        std::string name;
+
+        /// Sorting position of the channel.
+        unsigned position;
+
+        /// Explicit permission overwrites for members and roles.
+        std::vector<PermissionsOverwrite> permissionOverwrites;
+    protected:
+        BaseGuildChannel() = default;
+        BaseGuildChannel(const nlohmann::json& json);
+
+        nlohmann::json dump() const;
+    };
+
+    struct TextChannel : BaseGuildChannel {
+        TextChannel() = default;
+        TextChannel(const nlohmann::json& json);
+
+        nlohmann::json dump() const;
+
+        /// The channel topic (0-1024 characters).
+        std::string topic = "";
+
+        /// The id of the last message sent in this channel (may not point to an existing or valid message).
+        Snowflake lastMessageId;
+    };
+
+    struct VoiceChannel : BaseGuildChannel {
+        VoiceChannel() = default;
+
+        /// Construct from JSON. For internal use only.
+        VoiceChannel(const nlohmann::json& json);
+
+        /// Serialize to JSON. For internal use only.
+        nlohmann::json dump() const;
+
+        /// The bitrate (in bits) of the voice channel.
+        unsigned bitrate    = 64000;
+
+        /// The users limit of the voice channel.
+        unsigned usersLimit = 0;
+    };
+
+
+    struct Category : BaseGuildChannel {
+        // TODO: Should we make Category default-constructible?
+        Category() = default;
+
+        /// Construct from JSON. For internal use only.
+        Category(const nlohmann::json& json);
+
+        /// Serialize to JSON. For internal use only.
+        nlohmann::json dump() const;
+
+
+        bool nsfw;
+    };
+
+    struct DmChannel {
+        // TODO: Should we make DmChannel default-constructible?
+        DmChannel() = delete;
+
+        /// Construct from JSON. For internal use only.
+        DmChannel(const nlohmann::json& json);
+
+        // Serialize to JSON. For internal use only.
+        nlohmann::json dump() const;
+
+        /// DM ID.
+        Snowflake id;
+
+        /// The id of the last message sent in this channel (may not point to an existing or valid message).
+        Snowflake lastMessageId;
+
+        /// The recipient of the DM.
+        User recipient;
+    };
+
+    struct GroupDmChannel {
+        // TODO: Should we make GroupDmChannel default-constructible?
+        GroupDmChannel() = delete;
+
+        /// Construct from JSON. For internal use only.
+        GroupDmChannel(const nlohmann::json& json);
+
+        /// Serialize to JSON. For internal use only.
+        nlohmann::json dump() const;
+
+        // Group DM ID.
+        Snowflake id;
+
+        /// The id of the last message sent in this channel (may not point to an existing or valid message).
+        Snowflake lastMessageId;
+
+        /// Application id of the group DM creator if it is bot-created, 0 otherwise.
+        Snowflake applicationId;
+
+        /// The recipients of the DM.
+        std::vector<User> recipients;
+    };
+
+    enum class ChannelType {
+        Text            = 0,
+        Voice           = 2,
+        Category        = 4,
+        Dm              = 1,
+        GroupDm         = 3
+    };
+
+    ChannelType channelType(const nlohmann::json& json);
+
+    template<typename T>
+    constexpr ChannelType channelType();
+
+    template<> constexpr ChannelType channelType<TextChannel>()    { return ChannelType::Text;     }
+    template<> constexpr ChannelType channelType<VoiceChannel>()   { return ChannelType::Voice;    }
+    template<> constexpr ChannelType channelType<Category>()       { return ChannelType::Category; }
+    template<> constexpr ChannelType channelType<DmChannel>()      { return ChannelType::Dm;       }
+    template<> constexpr ChannelType channelType<GroupDmChannel>() { return ChannelType::GroupDm;  }
+
+    using Channel = boost::variant<TextChannel, VoiceChannel, Category, DmChannel, GroupDmChannel>;
+
+    void from_json(const nlohmann::json& json, Channel& channel);
+    void to_json(nlohmann::json& json, const Channel& channel);
+
+    HEXICORD_ENABLE_IMPLICIT_JSON_CONVERSIONS(TextChannel)
+    HEXICORD_ENABLE_IMPLICIT_JSON_CONVERSIONS(VoiceChannel)
+    HEXICORD_ENABLE_IMPLICIT_JSON_CONVERSIONS(DmChannel)
+    HEXICORD_ENABLE_IMPLICIT_JSON_CONVERSIONS(GroupDmChannel)
+} // namespace Hexicord
+
+#endif // HEXICORD_TYPES_CHANNEL_HPP
